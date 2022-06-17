93fa7b800d8f346afd8032a82f1e3bcee69ba377
xchagasx <xchagasx@yahoo.com.br>
2022-06-17 18:01:28 -0300

    ex3..

---
+++ Course/Day1/new-project-ex3/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Content from './Content';
+
+function App() {
+  return (
+    <Header />,
+    <Content />
+  );
+}
+
+export default App;
