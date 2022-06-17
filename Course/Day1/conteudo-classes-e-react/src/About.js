f30adf6e03237aa7f139efcf8bcee305006f0c3c
xchagasx <xchagasx@yahoo.com.br>
2022-06-17 16:51:11 -0300

    conteudo classes e react

---
+++ Course/Day1/conteudo-classes-e-react/src/About.js
@@ -0,0 +1,12 @@
+
+function About() {
+    return (
+        <div>
+            <h1>
+                <p>Funny and Happy, is amanzing</p>
+            </h1>
+        </div>
+    )
+}
+
+export default About;
