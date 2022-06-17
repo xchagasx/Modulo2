cb5b6df63e193da859175d4d8cfce81676fb1442
xchagasx <xchagasx@yahoo.com.br>
2022-06-17 17:26:24 -0300

    ex 2

--- Course/Day1/familychagas-app/src/App.js
+++ Course/Day1/familychagas-app/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
-function App() {
+const Task = (value) => {
   return (
-    <h1>Hello World!!!</h1>
-    )
+    <li key={value}>{value}</li>
+  )
+}
+
+const tarefas = ['Descansar', 'Ler', 'Estudar', 'Aplicar'];
+
+class App extends React.Component {
+  render() {
+    return (
+      <ol>{ tarefas.map(tarefas => Task(tarefas)) }</ol>
+    );
   }
+}
+
   export default App;
   
