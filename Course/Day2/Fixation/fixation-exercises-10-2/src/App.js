ec26b003d6e00bc1ae4ae6f619d5901257948c18
xchagasx <xchagasx@yahoo.com.br>
2022-06-20 14:04:16 -0300

    exersices fixaxion 1

---
+++ Course/Day2/Fixation/fixation-exercises-10-2/src/App.js
@@ -0,0 +1,38 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import cat from './cat.jpg'
+import Image from './Image';
+
+function App() {
+  return (
+    <main>
+      <Image source={cat} alternativeText='Cute cat staring' />
+    </main>
+  );
+  // <Image />;
+}
+
+export default App;
